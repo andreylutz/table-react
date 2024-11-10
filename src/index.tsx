d73a87331fab1885c6bd1d289492e6d5cebf0f15
@a.lucenko <a.lucenko@promo-bot.ru>
2024-11-10 23:35:24 +0500

    feat(saved): Добавлено сохранение данных при перезагрузке страницы.

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,16 @@
 import './styles/index.scss';
 import App from './App';
-import store from './store';
 import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react';
+import { store, persistor } from './store';
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 
 root.render(
-  <Provider store={store}>
-    <App />
-  </Provider> 
+  <PersistGate loading={null} persistor={persistor}>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </PersistGate>
 );
